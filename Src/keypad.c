41a88dd60e96d6c0656e77e75d6f97daf6a9ceca
rconmine <rconmine@gmail.com>
2020-11-11 21:50:08 +0300

    Ready keypad

--- Src/keypad.c
+++ Src/keypad.c
@@ -1,4 +1,6 @@
 #include "keypad.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_rcc.h"
 
 /// @ingroup Keypad
 /// @{
@@ -11,7 +13,20 @@ Keypad g_Keypad;
 
 Keypad* Keypad_Init() {
   // TODO! Init
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+  GPIO_InitTypeDef gpio = {0};
+  gpio.Pin = GPIO_PIN_1;
+  gpio.Mode = GPIO_MODE_IT_FALLING;
+  gpio.Pull = GPIO_PULLUP;
+  gpio.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(GPIOC, &gpio);
+	HAL_NVIC_EnableIRQ(EXTI1_IRQn);
   return &g_Keypad;
+}
+
+void EXTI1_IRQHandler(void){
+	EXTI->PR = EXTI_PR_PR1;
+	g_Keypad.keystates[KeyStartStop] = Clicked;
 }
 
 KeyState Keypad_GetState(Keypad* keypad, KeyButtons key) { return Unpressed; }
